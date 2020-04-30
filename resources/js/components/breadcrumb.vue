d10ca2fd3b93452a4243236390845d554dcda8e0
Jerick <zjerick.labasan@mynt.xyz>
2020-04-30 10:55:18 +0800

    fix minor bug

--- resources/js/components/breadcrumb.vue
+++ resources/js/components/breadcrumb.vue
@@ -1,19 +1,19 @@
-<template>
-    <ol class="breadcrumb">
-        <li v-for="(item, i) in items" :key="i" class="breadcrumb-item">
-            <router-link :to="item.link">{{ item.text }}</router-link>
-        </li>
-    </ol>
+<template functional>
+  <ol class="breadcrumb">
+    <li v-for="(item, i) in props.items" :key="i" class="breadcrumb-item">
+      <router-link :to="item.link">{{ item.text }}</router-link>
+    </li>
+  </ol>
 </template>
 
 <script>
 export default {
-    props: {
-        items: {
-            required: true,
-            type: Array
-        }
+  props: {
+    items: {
+      required: true,
+      type: Array
     }
+  }
 };
 </script>
 
